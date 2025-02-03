b289cb7c3f24531a5089bbec5651f89420ea9b64
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-02-03 15:41:45 -0700

    c

---
+++ TradingBot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class TradingBot(gym.Env):
+
+    def __init__(self,data, env, amount = 10000):
+        self.env = env
+        
