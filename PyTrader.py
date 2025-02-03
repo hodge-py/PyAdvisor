13ee3ad586a102befcd63d47d9922b3315629ba4
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-02-03 02:01:29 -0700

    c

--- PyTrader.py
+++ PyTrader.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 import os
-import time
+from ta.volatility import BollingerBands
 
 class PyTrader:
 
@@ -39,9 +39,10 @@ class PyTrader:
             except:
                 financialPeg = None
 
-            arr.append([stock,priceBook,priceEarnings,financialPeg])
+            links = f"https://finance.yahoo.com/quote/{stock}"
+            arr.append([stock,priceBook,priceEarnings,financialPeg,links])
 
-        df = pd.DataFrame(arr, columns=['Stock','Price/Book','Price/Earnings',"Trailing Peg Ratio"])
+        df = pd.DataFrame(arr, columns=['Stock','Price/Book','Price/Earnings',"Trailing Peg Ratio", "Link"])
         print(df)
         if save_as_csv:
             df.to_csv("stocks.csv", index=True)
@@ -68,33 +69,31 @@ class PyTrader:
 
         return UnderValued
 
-    def generateTechnicalReport(self, stocks, shortWin = 50, longWin = 200, save_as_csv=False):
+    def generateSMAReport(self, stocks, shortWin = 50, longWin = 200, save_as_csv=False):
         allTechnicals = []
         for z in stocks:
             data = yf.Ticker(z)
             data = data.history(period="1y")
             data["SMA_50"] = data["Close"].rolling(window=shortWin).mean()
             data["SMA_200"] = data["Close"].rolling(window=longWin).mean()
-
-            # Plot the dat
-            plt.figure(figsize=(12, 6))
-            plt.plot(data["Close"], label="Stock Price", alpha=0.6)
-            plt.plot(data["SMA_50"], label=f"{shortWin}-Day SMA", linestyle="dashed")
-            plt.plot(data["SMA_200"], label=f"{longWin}-Day SMA", linestyle="dotted")
-            plt.legend()
-            plt.title(f"{z} Moving Average Crossover")
-            plt.show()
+            #indicator_bb = BollingerBands(close=data["Close"], window=50,window_dev=2)
 
             sma = ''
-            if (data["SMA_50"].iloc[-1] > data["SMA_200"].iloc[-1]) and (abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]) <= 0.01):
-                sma = "Buy"
-                #print(abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]))
-            elif ((data["SMA_50"].iloc[-1] < data["SMA_200"].iloc[-1]) and (abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]) <= 0.01)):
-                sma = "Sell"
-                #print("Sell " + str(z))
-            else:
-                sma = "Hold/Do Nothing"
-                #print("Hold/Do Nothing " + str(z))
+            try:
+                if (data["SMA_50"].iloc[-1] > data["SMA_200"].iloc[-1]) and (0 <= abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]) <= .01):
+                    sma = "Examine"
+                    self._generateSMAGraph(data, z,shortWin, longWin)
+                    print(abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]))
+                elif (data["SMA_50"].iloc[-1] < data["SMA_200"].iloc[-1]) and (0 <= abs((data["SMA_50"].iloc[-1] - data["SMA_200"].iloc[-1]) / data["SMA_50"].iloc[-1]) <= 0.01):
+                    sma = "Examine"
+                    #print("Sell " + str(z))
+                    self._generateSMAGraph(data, z,shortWin, longWin)
+                else:
+                    sma = "Hold/Do Nothing"
+                    #print("Hold/Do Nothing " + str(z))
+
+            except:
+                sma = None
 
 
             allTechnicals.append([z,sma])
@@ -105,3 +104,19 @@ class PyTrader:
 
         if save_as_csv:
             df2.to_csv("stocksTechnical.csv", index=True)
+
+
+
+    def _generateSMAGraph(self,data,z,shortSma,longSma):
+        # Plot the data
+        plt.figure(figsize=(12, 6))
+        plt.plot(data["Close"], label="Stock Price", alpha=0.6)
+        plt.plot(data["SMA_50"], label=f"{shortSma}-Day SMA", linestyle="dashed",color="red")
+        plt.plot(data["SMA_200"], label=f"{longSma}-Day SMA", linestyle="dotted", color="green")
+        plt.legend()
+        plt.title(f"{z} Moving Average Crossover")
+        plt.show()
+
+
+    def combineTables(self):
+        pass
