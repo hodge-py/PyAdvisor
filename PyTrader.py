05a368ac89686065be573f7764f7619f64b10831
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-02-02 23:11:55 -0700

    c

--- PyTrader.py
+++ PyTrader.py
@@ -2,14 +2,49 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import os
+import time
 
 class PyTrader:
 
     def __init__(self):
         pass
 
-    def generateValueReport(self,stocks):
-        pass
+    def generateValueReport(self,stocks,save_as_csv=False):
+        arr = []
+        for stock in stocks:
+            y = yf.Ticker(stock)
+            try:
+                closePrice = y.history(period="1mo")['Close'].iloc[[-1]]
+            except:
+                closePrice = 0
+
+            try:
+                financialsEPS = y.info['trailingEps']
+                priceEarnings = closePrice.iloc[0] / financialsEPS
+            except:
+                priceEarnings = None
+            try:
+                quarters = y.quarterly_balance_sheet.iloc[:, 0]
+                priceBook = closePrice.iloc[0] / (quarters['Tangible Book Value'] /
+                                              quarters['Share Issued'])
+            except:
+                priceBook = None
+
+            try:
+                financialPeg = y.info['trailingPegRatio']
+            except:
+                financialPeg = None
+
+            arr.append([stock,priceBook,priceEarnings,financialPeg])
+
+        df = pd.DataFrame(arr, columns=['Stock','Price/Book','Price/Earnings',"Trailing Peg Ratio"])
+        print(df)
+        if save_as_csv:
+            df.to_csv("stocks.csv", index=True)
+
 
     def stockScreener(self, stocks, PB = 5, PE = 5):
         UnderValued = []
@@ -21,8 +56,14 @@ class PyTrader:
             if priceBook < PB:
                 UnderValued.append(z)
 
-
         print("UnderValued Stocks: " + str(UnderValued))
+
+        # Saves undervalued stocks to clip board to use for generate ValueReport
+        def addToClipBoard(text):
+            command = 'echo ' + text.strip() + '| clip'
+            os.system(command)
+
+        addToClipBoard(str(UnderValued))
 
 
     def generateTechnicalReport(self, stocks, shortWin = 50, longWin = 200):
@@ -32,7 +73,7 @@ class PyTrader:
             data["SMA_50"] = data["Close"].rolling(window=shortWin).mean()
             data["SMA_200"] = data["Close"].rolling(window=longWin).mean()
 
-            # Plot the data
+            # Plot the dat
             plt.figure(figsize=(12, 6))
             plt.plot(data["Close"], label="Stock Price", alpha=0.6)
             plt.plot(data["SMA_50"], label=f"{shortWin}-Day SMA", linestyle="dashed")
@@ -48,3 +89,7 @@ class PyTrader:
                 print("Sell " + str(z))
             else:
                 print("Hold/Do Nothing " + str(z))
+
+
+
+print(yf.Ticker('AACT').info['trailingEps'])
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import PyTrader
 
 pyT = PyTrader.PyTrader()
-pyT.stockScreener(["MSFT","FINV", "TSLA"])
+"""
+pyT.stockScreener(['A','AA','AACG','AACT','AADI','AAL','AAM','AAME','AAMI','AAOI','AAON','AAP',
+                   'AAPG','AAPL','AAT','AB','ABAT','ABBV','ABCB','ABCL','ABEO','ABEV','ABG','ABL','ABLV','ABM','ABNB','ABOS','ABP','ABR','ABSI',
+                   'ABT','ABTS','ABUS','ABVC','ABVE','ABVX','AC','ACA','ACAD','ACB','ACCD','ACCO','ACCS','ACDC','ACEL','ACET','ACGL','ACHC','ACHL','ACHR','ACHV',
+                   'ACI','ACIC','ACIU','ACIW','ACLS','ACLX','ACM','ACMR','ACN','ACNB','ACNT','ACOG','ACON','ACR','ACRE','ACRS','ACRV','ACT','ACTG',
+                   'ACTU','ACU','ACVA','ACXP','ADAG','ADAP','ADBE','ADC','ADCT','ADD','ADEA','ADGM','ADI','ADIL','ADM','ADMA'])
+"""
+pyT.generateValueReport(['MSFT','NVDA','AMZN','GOOGL','META'],save_as_csv=True)
 
-pyT.generateTechnicalReport(["MSFT","FINV","VRRM"])
+
+#pyT.generateTechnicalReport(['TER'])
