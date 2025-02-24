6bb61b4f981747ca3a5c92114eb4c69c1b5e35fa
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-02-23 22:23:27 -0700

    c

--- roboAdvisor.py
+++ roboAdvisor.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+from scipy.optimize import minimize
+from tensorflow.python.ops.gen_array_ops import lower_bound
 
 
 class RoboAdvisor:
@@ -15,12 +17,16 @@ class RoboAdvisor:
         self.names = 0
         self.returns = []
         self.volatility = []
+        self.args = (0,0)
+        self.optimal_weight = []
 
         self.days = np.busday_count(self.timePeriod,dt.date.today(),weekmask="Mon Tue Wed Thu Fri")
 
         self._stockHistory()
         self._meanVariance()
         self._calculate_random_portfolio()
+        self._minimize_portfolio()
+        self._plot_optimal_portfolio()
 
     def _stockHistory(self):
         df = yf.download(tickers=self.tickers, start=self.timePeriod)
@@ -54,6 +60,39 @@ class RoboAdvisor:
         plt.scatter(self.volatility, self.returns)
         plt.show()
 
+    def _portfolio_mean(self, weight):
+        mean_r = self.args[0]
+        port_return = np.dot(weight,mean_r)
+        return port_return
 
+    def _portfolio_std(self, weight,*args):
+        std_r = self.args[1]
+        port_std = np.sqrt(np.dot(weight.T,np.dot(std_r,weight)))
+        return port_std
 
-r = RoboAdvisor(['MSFT','AAPL','ADBE','META'],'2024-01-01')
+    def _minimize_portfolio(self):
+        lower_bound = 0
+        upper_bound = 1
+        bounds = ()
+        for i in range(len(self.names)):
+            bounds += ((lower_bound,upper_bound),)
+        initial_weight = np.ones(len(self.names))/len(self.names)
+        print(bounds)
+        self.args = (self.mean,self.variance)
+        target = 0.15
+        constraints = ({"type": 'eq', 'fun': lambda x: self._portfolio_mean(x) - target},
+                       {'type': 'eq', 'fun': lambda x: np.sum(x)-1})
+
+        results = minimize(self._portfolio_std, initial_weight, args=self.args, method='SLSQP', bounds=bounds, constraints=constraints)
+        self.optimal_weight = results.x
+        print(self.optimal_weight)
+
+    def _plot_optimal_portfolio(self):
+        optimal_portfolio_plot = np.dot(self.df_norm,self.optimal_weight)
+        equal_weight_p = np.dot(self.df_norm,np.ones(len(self.names))/len(self.names))
+        plt.plot(optimal_portfolio_plot,label='Optimal Portfolio (15%)')
+        plt.plot(equal_weight_p,label='Equal Weight')
+        plt.legend(loc='best')
+        plt.show()
+
+r = RoboAdvisor(['NBET','MITA','AVRE'],'2024-01-01')
